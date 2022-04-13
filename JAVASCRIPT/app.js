291a39f823250b41beceb28caa47809b5e6f51a6
Barry-owino <barryowino5@gmail.com>
2022-04-13 06:39:10 +0300

    added the validateName function and the check icon in the css and html

--- JAVASCRIPT/app.js
+++ JAVASCRIPT/app.js
@@ -3,6 +3,19 @@ const phoneError = document.getElementById('phone-error');
 const emailError = document.getElementById('email-error');
 const messageError = document.getElementById('message-error');
 
-function validateForm() {
-  
+function validateName() {
+  const name = document.getElementById('contact-name').value;
+
+  if (name.length == 0){
+    nameError.innerHTML = 'Name is required';
+    return false;
+  }
+  if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+    nameError.innerHTML = 'Write full name';
+    return false;
+  }
+  nameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+  return true;
+
+
 }
